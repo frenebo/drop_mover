cf2f6a831c5c76c695d4584c7e9543f1013c67bf
Paul Kreymborg <frenebo@gmail.com>
2022-04-06 16:40:03 -0700

    swap dimensions to try  to fix this

--- scripts/paint_bubbles.py
+++ scripts/paint_bubbles.py
@@ -95,8 +95,9 @@ def make_bubble_ramps(im, image_background=None, invert_BW=False, sigma_for_edge
         bubble_start_x = np.min(region_border_coords_xy[1])
         bubble_end_x = np.max(region_border_coords_xy[1])
         ramp = np.linspace(0,MAX_BRIGHTNESS, bubble_end_x - bubble_start_x,dtype=bubble_paint.dtype)
-        ramp = np.tile(np.transpose(ramp), (img_height,1))
+        ramp = np.tile(ramp, (1,img_height))
+        # print("Ramp shape: ")
         ramp_mask = np.zeros_like(bubble_paint)
-        ramp_mask[:,bubble_start_x:bubble_end_x] = ramp
+        ramp_mask[bubble_start_x:bubble_end_x,:] = ramp
 
         bubble_paint += ramp_mask * bubble_bin_mask
