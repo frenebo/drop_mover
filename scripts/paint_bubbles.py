55bcfce72d5b0e949afbde7de483c167738c5d97
Paul Kreymborg <frenebo@gmail.com>
2022-04-06 16:42:49 -0700

    print more info

--- scripts/paint_bubbles.py
+++ scripts/paint_bubbles.py
@@ -95,7 +95,9 @@ def make_bubble_ramps(im, image_background=None, invert_BW=False, sigma_for_edge
         bubble_start_x = np.min(region_border_coords_xy[1])
         bubble_end_x = np.max(region_border_coords_xy[1])
         ramp = np.linspace(0,MAX_BRIGHTNESS, bubble_end_x - bubble_start_x,dtype=bubble_paint.dtype)
+        print("Ramp width:  {}".format(ramp.shape))
         ramp = np.tile(ramp, (img_height,1))
+        print("Tiled ramp shape:  {}".format(ramp.shape))
         # print("Ramp shape: ")
         ramp_mask = np.zeros_like(bubble_paint)
         ramp_mask[bubble_start_x:bubble_end_x,:] = ramp
