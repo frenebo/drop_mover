4a50a6f888a2620eb67e2cfd304c2f0aa9092a45
Paul Kreymborg <frenebo@gmail.com>
2022-04-06 16:51:46 -0700

    add test image

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pycromanager import Bridge
 import numpy as np
+from PIL import Image
 from scripts.utilities import send_slm_image, get_slm_info, read_image
 from scripts.paint_bubbles import make_bubble_ramps
-
 
 
 def main():
@@ -19,9 +19,12 @@ def main():
         microscope_image = read_image(core)
         print("Image dimensions: {}".format(microscope_image.shape))
 
-        bubble_ramp_image = make_bubble_ramps(microscope_image)
+        tif_img = np.array(Image.open("img_0000.tif"))
+
+        bubble_ramp_image = make_bubble_ramps(tif_img)
         assert bubble_ramp_image.dtype == np.uint8, "Bubble ramp image should be 8 bit to work right with PIL Image"
         bubble_ramp_pil_img = Image.fromarray(bubble_ramp_image, 'L')
+        bubble_ramp_pil_img.save("data/image.tif")
 
         project_image = Image.new('L', (slm_height, slm_width))
         project_image = project_image.resize([int(camera_slm_zoom_ratio * s) for s in im.size])
