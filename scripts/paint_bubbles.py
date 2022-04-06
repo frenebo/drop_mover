50c75888d1a783c4f97ad2f68dbee4d32575bbbe
Paul Kreymborg <frenebo@gmail.com>
2022-04-06 16:46:58 -0700

    print start and end x for bubble

--- scripts/paint_bubbles.py
+++ scripts/paint_bubbles.py
@@ -99,6 +99,7 @@ def make_bubble_ramps(im, image_background=None, invert_BW=False, sigma_for_edge
         ramp = np.transpose(np.tile(ramp, (img_height,1)))
         print("Tiled ramp shape:  {}".format(ramp.shape))
         print("Image shape: {}".format(bubble_paint.shape))
+        print("bubble start x: {} end x: {}".format(bubble_start_x, bubble_end_x))
         # print("Ramp shape: ")
         ramp_mask = np.zeros_like(bubble_paint)
         ramp_mask[bubble_start_x:bubble_end_x,:] = ramp
